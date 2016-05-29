13d24e22c35e23c82e4b2bfccd04282f42e71a3c
João Victor Moreira <jvrodriguesmoreira@gmail.com>
2016-05-28 23:28:55 -0300

    Keynote speakers

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
 
+
 class HomeCase(TestCase):
 
     def setUp(self):
@@ -16,4 +17,21 @@ class HomeCase(TestCase):
 
     def test_subscription_link(self):
         expected = 'href="{}'.format(r('subscriptions:new'))
+        self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        """Must show keynote speakers"""
+        contents = [
+            'Grace Hopper',
+            'http://hbn.link/hopper-pic',
+            'Alan Turing',
+            'http://hbn.link/turing-pic'
+        ]
+
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
         self.assertContains(self.response, expected)
